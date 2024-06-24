0f2c02273ef64beb593b7a49a7f31b8e3de5302d
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-06-24 12:38:34 +0700

    Goofy ahh layout

--- app/src/main/java/com/example/andstore/MainActivity.java
+++ app/src/main/java/com/example/andstore/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.andstore;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
