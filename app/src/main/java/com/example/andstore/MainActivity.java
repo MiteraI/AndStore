913af024ceb122ba8d087fa38e99b1f117835105
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-06-25 00:23:33 +0700

    Item card and carousel

--- app/src/main/java/com/example/andstore/MainActivity.java
+++ app/src/main/java/com/example/andstore/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andstore;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Banner slider settings
+        ArrayList<SlideModel> imageList = new ArrayList<SlideModel>(); // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(new SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years.", ScaleTypes.FIT));
+        imageList.add(new SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct.", ScaleTypes.FIT));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72", "And people do that.", ScaleTypes.FIT));
+
+        ImageSlider imageSlider = (ImageSlider) findViewById(R.id.banner_slider);
+                imageSlider.setImageList(imageList);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         EdgeToEdge.enable(this);
